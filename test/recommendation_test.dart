7904671f0fa8878c9454d4e39071f4c47eadf127
Dafa15 <dafaaldian25@gmail.com>
2024-05-13 23:28:17 +0700

    fix: UI test error in recommendation page

--- test/recommendation_test.dart
+++ test/recommendation_test.dart
@@ -3,13 +3,22 @@ import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mangaland_flutter/env/env.dart';
 import 'package:mangaland_flutter/page/recommendation/recommend_page.dart';
+import 'package:mangaland_flutter/page/recommendation/recommendation_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('RecommendationPage UI Test', (WidgetTester tester) async {
     Gemini.init(apiKey: Env.geminiApiKey);
 
-    await tester.pumpWidget(const MaterialApp(
-      home: RecommendationPage(),
+    await tester.pumpWidget(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => RecommendationViewModel(),
+        ),
+      ],
+      child: const MaterialApp(
+        home: RecommendationPage(),
+      ),
     ));
 
     final titleFinder = find.text("Get manga recommendation!!");
