5105febb30602cc1bf95201c14cb17a18b1fc15e
Dafa15 <dafaaldian25@gmail.com>
2024-05-13 23:17:34 +0700

    add: image_constant for store image path

---
+++ lib/constant/image_constant.dart
@@ -0,0 +1,6 @@
+class ImageConstant {
+  static String luffyImg = "assets/luffy.png";
+  static String iconApp = 'assets/mangaland_logo.png';
+  static String chainsawLottie = 'assets/chainsaw.json';
+  static String searchLottie = 'assets/search.json';
+}
